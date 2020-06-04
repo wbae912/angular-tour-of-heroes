1e3cd4de620f6638c376e0178f8bb3a9657dd863
William Bae <wbae@lacourt.org>
2020-06-04 15:05:40 -0700

    Created a HeroService and injected it into the HeroComponent...mock data is being pulled into the service and delivered to the component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HeroService } from './hero.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
